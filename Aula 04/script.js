93763df192bc5b54a22b6b334c94fffca77a2b6c
NAILTON SANTOS <nltsilva8@gmail.com>
2022-06-24 06:06:30 -0400

    Initial commit

---
+++ Aula 04/script.js
@@ -0,0 +1,10 @@
+//Comentário de uma linha
+/*
+Comentário de Várias Linhas
+*/
+
+//Statements - instruções; podem ser de uma linha ou mais
+//Valores, operadores, expressões, palavras-chave e comentários
+
+console.error('Deu ruim')
+console.log('Nailton Silva')
